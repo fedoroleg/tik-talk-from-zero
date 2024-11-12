396f7fc6eccffad849609989591d335ffd6afddc
fedoroleg <fedoroleg@gmail.com>
2024-11-12 15:33:15 +0300

    feat(settings-page): add drag and drop upload

--- src/app/pages/settings-page/avatar-upload/avatar-upload.component.ts
+++ src/app/pages/settings-page/avatar-upload/avatar-upload.component.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { SvgIconComponent } from "../../../common-ui/svg-icon/svg-icon.component";
+import { DragAndDropDirective } from '../../../common-ui/directives/drag-and-drop.directive';
 
 @Component({
   selector: 'app-avatar-upload',
   standalone: true,
-  imports: [SvgIconComponent],
+  imports: [SvgIconComponent, DragAndDropDirective],
   templateUrl: './avatar-upload.component.html',
   styleUrl: './avatar-upload.component.scss'
 })
@@ -13,23 +14,20 @@ export class AvatarUploadComponent {
 
   fileBrowserHandler(event: Event) {
     const file = (event.target as HTMLInputElement)?.files?.[0]
-    
-    console.log(event);
-    
-    console.log(file);
-    console.log(file?.type.match('image'));
+    this.processFile(file)
+  }
 
+  onFileDropped(file: File) {
+    this.processFile(file)
     
-    
+  }
+
+  processFile(file: File | null | undefined) {
     if (!file || !file.type.match('image')) return
-
     const reader = new FileReader()
-
     reader.onload = event => {
       this.preview.set(event.target?.result?.toString() || '')
     }
-
     reader.readAsDataURL(file)
-    
   }
 }
