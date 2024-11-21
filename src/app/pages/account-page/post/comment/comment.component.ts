8cc82341c03e13144645fe522423b99c04aaf3d1
fedoroleg <fedoroleg@gmail.com>
2024-11-21 13:55:54 +0300

    feat(post): add show fresh comment immediately + raw version of pipe customDate

--- src/app/pages/account-page/post/comment/comment.component.ts
+++ src/app/pages/account-page/post/comment/comment.component.ts
@@ -2,11 +2,12 @@ import { Component, Input } from '@angular/core';
 import { AvatarCircleComponent } from "../../../../common-ui/avatar-circle/avatar-circle.component";
 import { Comment } from '../../../../data-access/interfaces/post.interfaces';
 import { CommonModule } from '@angular/common';
+import { CustomDate } from "../../../../helpers/pipes/custom-date.pipe";
 
 @Component({
   selector: 'app-comment',
   standalone: true,
-  imports: [AvatarCircleComponent, CommonModule],
+  imports: [AvatarCircleComponent, CommonModule, CustomDate],
   templateUrl: './comment.component.html',
   styleUrl: './comment.component.scss'
 })
