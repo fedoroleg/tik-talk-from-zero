24696f68cef5da6691c70918bd9a3efa92aec1da
fedoroleg <fedoroleg@gmail.com>
2024-11-25 14:12:39 +0300

    feat(chats-list-item): add chats-list-item + mock data there

--- src/app/pages/chats-page/chats-list/chats-list-item/chats-list-item.component.ts
+++ src/app/pages/chats-page/chats-list/chats-list-item/chats-list-item.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { AvatarCircleComponent } from "../../../../common-ui/avatar-circle/avatar-circle.component";
 
 @Component({
   selector: 'app-chats-list-item',
   standalone: true,
-  imports: [],
+  imports: [AvatarCircleComponent],
   templateUrl: './chats-list-item.component.html',
   styleUrl: './chats-list-item.component.scss'
 })
