72d34db24b871b363d43e35ce5359b17b9bee524
fedoroleg <fedoroleg@gmail.com>
2024-12-23 15:47:07 +0300

    feat(posts): add loading status to posts feed

--- libs/posts/src/lib/feature-posts/posts-feed/posts-feed.component.ts
+++ libs/posts/src/lib/feature-posts/posts-feed/posts-feed.component.ts
@@ -4,14 +4,15 @@ import { PostComponent } from '../post/post.component';
 import { PostsService } from '../../data-access/posts-service.service';
 import { firstValueFrom } from 'rxjs';
 import { Store } from '@ngrx/store';
-import { selectPosts } from '../../data-access/posts.selectors';
+import { LetDirective } from '@ngrx/component';
+import { selectLoadingStatus, selectPosts } from '../../data-access/posts.selectors';
 import { postsActions } from '../../data-access/posts.actions';
 import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-posts-feed',
   standalone: true,
-  imports: [PostInputComponent, PostComponent, AsyncPipe],
+  imports: [PostInputComponent, PostComponent, AsyncPipe, LetDirective],
   templateUrl: './posts-feed.component.html',
   styleUrl: './posts-feed.component.scss',
 })
@@ -20,6 +21,7 @@ export class PostsFeedComponent {
   private readonly store = inject(Store);
   public posts = this.postsService.posts;
   public posts$ = this.store.select(selectPosts)
+  public readonly loadingStatus$ = this.store.select(selectLoadingStatus)
 
   constructor() {
     //firstValueFrom(this.postsService.getPosts());
