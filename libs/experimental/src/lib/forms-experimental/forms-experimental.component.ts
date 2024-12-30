82ebf7392a6287de62df38194bd7eef4bc004298
fedoroleg <fedoroleg@gmail.com>
2024-12-30 14:07:57 +0300

    video 02 fin

--- libs/experimental/src/lib/forms-experimental/forms-experimental.component.ts
+++ libs/experimental/src/lib/forms-experimental/forms-experimental.component.ts
@@ -1,5 +1,17 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import {
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+
+enum ReciverType {
+  PERSON = 'PERSON',
+  LEGAL = 'LEGAL',
+}
 
 @Component({
   selector: 'app-forms-experimental',
@@ -10,5 +22,44 @@ import { FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class FormsExperimentalComponent {
-  public form = new FormGroup({});
+  public readonly reciverType = ReciverType;
+
+  public form = new FormGroup({
+    type: new FormControl<ReciverType>(ReciverType.PERSON),
+    name: new FormControl<string>('', [Validators.required]),
+    lastName: new FormControl<string>(''),
+    inn: new FormControl<number | null>(null),
+    address: new FormGroup({
+      city: new FormControl<string>(''),
+      street: new FormControl<string>(''),
+      building: new FormControl<number | null>(null),
+      apartment: new FormControl<number | null>(null),
+    }),
+  });
+
+  constructor() {
+    this.form.controls.type.valueChanges
+      .pipe(takeUntilDestroyed())
+      .subscribe((val) => {
+        console.log(val);
+
+        this.form.controls.type.clearValidators();
+
+        if (val === this.reciverType.LEGAL) {
+          this.form.controls.inn.setValidators([
+            Validators.required,
+            Validators.minLength(5),
+          ]);
+        }
+      });
+  }
+
+  onSubmit(event: Event) {
+    this.form.markAllAsTouched();
+    this.form.updateValueAndValidity();
+
+    if (this.form.invalid) return;
+    console.log(this.form.valid);
+    console.log(this.form.value);
+  }
 }
